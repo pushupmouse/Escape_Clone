f2f314c176845589cfd683f519066b06bf7bec41
pushupmouse <vutranhungduc@gmail.com>
2024-01-17 18:32:56 +0700

    board builder

--- Assets/Game/Scripts/Blocks/BoardBlock.cs
+++ Assets/Game/Scripts/Blocks/BoardBlock.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
+public class BoardBlock : MonoBehaviour
 {
-
+    public const float SIZE = 0.25f;
 }
